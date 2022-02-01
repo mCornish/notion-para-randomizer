fe4038599160744992d7671cba0b1bd687645f07
Mike Cornish <cornishmw@gmail.com>
2022-01-31 23:19:37 -0500

    🎉 Complete MVP

--- background.js
+++ background.js
@@ -1,33 +1,103 @@
-function login() {
-  chrome.identity.launchWebAuthFlow({
-    url: `https://api.notion.com/v1/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&owner=${OWNER}`,
-    interactive: true
+const logger = console;
+
+const CLIENT_ID = 'b3fdb0a2-b19b-4e14-b6ec-562d15089bb7';
+const REDIRECT_URI = chrome.identity.getRedirectURL();
+const RESPONSE_TYPE = 'code';
+const OWNER = 'user';
+const TOKEN_URL = 'https://api.notion.com/v1/oauth/token';
+const AUTH_CREDENTIAL = 'YjNmZGIwYTItYjE5Yi00ZTE0LWI2ZWMtNTYyZDE1MDg5YmI3OnNlY3JldF9hd3ljUGxiSDZoMklISjNETVRVeGFiYzhTc2NRVXQybGVhVEpybHpoNEFx';
+const GRANT_TYPE = 'authorization_code';
+
+async function completeAuth(redirectUrl) {
+  const query = redirectUrl.split('?')[1];
+  const code = new URLSearchParams(query).get('code');
+  if (!code) throw new Error('No code found in redirect URL.');
+
+  logger.debug(`Fetching access token for code ${code}`);
+  
+  const headers = new Headers();
+  headers.append("Authorization", `Basic ${AUTH_CREDENTIAL}`);
+  headers.append("Content-Type", "application/x-www-form-urlencoded");
+
+  const body = new URLSearchParams();
+  body.append("grant_type", GRANT_TYPE);
+  body.append("code", code);
+  body.append("redirect_uri", REDIRECT_URI);
+
+  const requestOptions = {
+    method: 'POST',
+    headers,
+    body,
+  };
+
+  const response = await fetch("https://api.notion.com/v1/oauth/token", requestOptions)
+    .catch(err => {
+      console.error(err);
+    });
+
+  const responseJson = await response.json();
+
+  // The stored code is no longer valid, so clear it. Or there is some other issue and we should try again.
+  if (responseJson.error) {
+    if (responseJson.error === 'invalid_grant') chrome.storage.sync.set({ redirectUrl: null });
+    else throw new Error(responseJson.error);
+  }
+
+  logger.debug('Successfully retrieved access token.');
+
+  chrome.storage.sync.set({
+    accessToken: responseJson.access_token,
+    user: responseJson.owner.user,
   });
 }
-chrome.runtime.onInstalled.addListener(async () => {
-  // const notion = new Client({ auth: process.env.NOTION_TOKEN });
 
-  // const PAGE_SIZE = 100;
+async function startAuth() {
+  const authUrl = `https://api.notion.com/v1/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&owner=${OWNER}`;
+  
+  logger.debug('launchWebAuthFlow', authUrl);
+  
+  chrome.identity.launchWebAuthFlow({
+    url: authUrl,
+    interactive: true
+  }, async (redirectUrl) => {
+    logger.debug('launchWebAuthFLow login successful: ', redirectUrl);
 
-  // async function getNextCursors(start_cursor) {
-  //   const searchResponse = await notion.search({
-  //     object: 'page',
-  //     page_size: PAGE_SIZE,
-  //     start_cursor
-  //   });
-    
-  //   if (searchResponse.has_more) {
-  //     // cursors.push(searchResponse.next_cursor);
-  //     return [start_cursor, ...(await getNextCursors(searchResponse.next_cursor))];
-  //   }
+    completeAuth(redirectUrl);
+  });
+}
 
-  //   return [start_cursor];
-  // }
+async function init() {
+  logger.debug('Initializing background script');
 
-  // const cursors = await getNextCursors();
-  // console.log("🚀 ~ file: index.js ~ line 28 ~ chrome.storage.sync.get ~ cursors", cursors)
+  const storage = await chrome.storage.sync.get();
+  logger.debug('Storage: ', storage);
 
-  // chrome.storage.sync.set({ cursors: cursors.join(',') });
-});
+  const accessToken = storage.accessToken;
+
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    logger.debug('Received message: ', request);
+  
+    try {
+      if (request.login) {
+        startAuth();
+      } else if (request.getAccessToken) {
+        logger.debug('getAccessToken: ', accessToken);
+        sendResponse(accessToken);
+      } else {
+        throw new Error(`Did not recognize message: ${request}`);
+      }
+    } catch (error) {
+      logger.error('Error while responding to message.', error);
+      sendResponse({ error });
+    }
+  });
+  
+  // chrome.runtime.onInstalled.addListener(async () => {
+  // });
+}
+
+init();
+
+
 
 
--- index.js
+++ index.js
@@ -1,65 +1,133 @@
-// const { Client } = require('@notionhq/client');
-// require('dotenv').config();
+const logger = console;
 
-const CLIENT_ID = 'b3fdb0a2-b19b-4e14-b6ec-562d15089bb7';
-const REDIRECT_URI = chrome.identity.getRedirectURL();
-const RESPONSE_TYPE = 'code';
-const OWNER = 'user';
-console.log("TEST")
 const authButton = document.getElementById('authenticate');
-authButton.addEventListener('click', async () => {
-  // chrome.runtime.getBackgroundPage().login();
-  const saveUrl = await chrome.storage.sync.get('redirectUrl');
-  console.log("🚀 ~ file: index.js ~ line 13 ~ authButton.addEventListener ~ saveUrl", saveUrl)
-  chrome.identity.launchWebAuthFlow({
-    url: `https://api.notion.com/v1/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&owner=${OWNER}`,
-    interactive: true
-  }, (redirectUrl) => {
-    chrome.storage.sync.set({ redirectUrl });
-  console.log("🚀 ~ file: index.js ~ line 16 ~ authButton.addEventListener ~ redirectUrl", redirectUrl)
+const noteButton = document.getElementById('getRandomNote');
+
+function updateUi(accessToken) {
+  logger.debug('Updating UI with access token: ', accessToken);
+
+  if (accessToken) {
+    authButton.hidden = true;
+    noteButton.hidden = false;
+  } else {
+    authButton.hidden = false;
+    noteButton.hidden = true;
+  }
+}
+
+async function init() {
+  let accessToken = null;
+  chrome.runtime.sendMessage({ getAccessToken: true }, (response) => {
+    updateUi(response);
+    accessToken = response;
   });
-});
 
-// (async () => {
-//   const notion = new Client({ auth: process.env.NOTION_TOKEN });
+  authButton.addEventListener('click', async () => {
+    logger.debug('Clicked auth button');
+    const response = chrome.runtime.sendMessage({ login: true }, updateUi);
+    if (response?.error) throw new Error('Failed to complete login.', response.error);
+  });
+  
+  noteButton.addEventListener('click', async () => {
+    logger.debug('Clicked note button');
+    if (!accessToken) throw new Error('Attempted to get random note without access token.');
+    const randomNote = await getRandomNote(accessToken);
+    window.open(randomNote.url, '_blank');
+  });
+}
 
-//   // We must include undefined as an option to account for the first page, for which we are provided no cursor
-//   // let cursors = [undefined]
-//   const PAGE_SIZE = 100;
+init();
 
-//   async function getNextCursors(start_cursor) {
-//     const searchResponse = await notion.search({
-//       object: 'page',
-//       page_size: PAGE_SIZE,
-//       start_cursor
-//     });
+function NotionClient({ auth: token }) {
+  const search = async (options) => {
+    try {
+      const response = await fetch('https://api.notion.com/v1/search', {
+        method: 'POST',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json',
+          'Notion-Version': '2021-08-16',
+        },
+        body: JSON.stringify(options),
+      });
+
+      const json = await response.json();
+      logger.debug('Search response: ', json);
+
+      if (json.object === 'error') throw new Error(json.message);
+      return json;
+    } catch(err) {
+      throw new Error(`Failed to fetch search results: ${err.message}`);
+    }
+  };
+
+  return {
+    search
+  };
+}
+
+async function getRandomNote(accessToken) {
+  const notion = NotionClient({ auth: accessToken });
+
+  // We must include undefined as an option to account for the first page, for which we are provided no cursor
+  // let cursors = [undefined]
+  const PAGE_SIZE = 100;
+
+  async function getNextCursor(start_cursor) {
+    const searchResponse = await notion.search({
+      filter: {
+        property: 'object',
+        value: 'page',
+      },
+      page_size: PAGE_SIZE,
+      start_cursor
+    });
     
-//     if (searchResponse.has_more) {
-//       // cursors.push(searchResponse.next_cursor);
-//       return [start_cursor, ...(await getNextCursors(searchResponse.next_cursor))];
-//     }
+    if (searchResponse.has_more) {
+      if (!searchResponse.next_cursor) throw new Error('No next cursor found.');
+      return [start_cursor, ...(await getNextCursor(searchResponse.next_cursor))];
+    }
 
-//     return [start_cursor];
-//   }
+    return [start_cursor];
+  }
 
-//   chrome.storage.sync.get('cursors', ({ cursors: storedCursors }) => {
-//     const cursors = storedCursors ? storedCursors.split(',') : getNextCursors();
-//     console.log("🚀 ~ file: index.js ~ line 28 ~ chrome.storage.sync.get ~ cursors", cursors)
+  return new Promise((resolve, reject) => {
+    try {
+      chrome.storage.sync.get('cursors', async ({ cursors: storedCursors }) => {
+        let cursors = [];
 
-//     chrome.storage.sync.set({ cursors: cursors.join(',') });
+        try {
+          // TODO: Use storage to optimize use of cursors
+          // cursors = storedCursors ? [undefined, ...storedCursors.split(',')] : await getNextCursor();
+          cursors = await getNextCursor();
+        } catch(err) {
+          logger.error('Failed to get cursors: ', err);
+          await chrome.storage.sync.set({ cursors: null });
+        }
 
-//     const randomCursor = cursors[Math.floor(Math.random() * (cursors.length - 1))];
+        console.log("🚀 ~ file: index.js ~ line 28 ~ chrome.storage.sync.get ~ cursors", cursors)
 
-//     const results = (await notion.search({
-//       object: 'page',
-//       page_size: PAGE_SIZE,
-//       start_cursor: randomCursor
-//     })).results;
+        chrome.storage.sync.set({ cursors: cursors.slice(1).join(',') });
 
-//     const randomResult = results[Math.floor(Math.random() * (results.length - 1))];
-//     let noteUrlElement = document.getElementById("noteUrlElement");
-//     noteUrlElement.innerHTML = randomResult.url;
-//     console.log("RANDOM NOTE URL:", randomResult.url)
-//   });
-//   await getNextCursor();
-// })();
+        const randomCursor = cursors[Math.floor(Math.random() * (cursors.length - 1))];
+        logger.debug("Got random cursor: ", randomCursor);
+
+        const results = (await notion.search({
+          filter: {
+            property: 'object',
+            value: 'page',
+          },
+          page_size: PAGE_SIZE,
+          start_cursor: randomCursor
+        })).results;
+
+        const randomResult = results[Math.floor(Math.random() * (results.length - 1))];
+        console.debug("Generated random note:", randomResult);
+
+        resolve(randomResult);
+      });
+    } catch(err) {
+      reject(err);
+    }
+  });
+}
